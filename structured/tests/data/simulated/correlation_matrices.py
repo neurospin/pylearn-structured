66a41f0904038e7e214c578e65007f8dc0d1e9f1
Tommy Löfstedt <tommy.loefstedt@cea.fr>
2013-06-20 10:38:20 +0200

    Simulated correlation matrices.

--- structured/tests/data/simulated/correlation_matrices.py
+++ structured/tests/data/simulated/correlation_matrices.py
@@ -2,17 +2,22 @@
 """
 Created on Wed Jun 19 13:56:24 2013
 
+Generates correlation matrices using two of the approaches described in:
+
+    Hardin & Garcia (2013). A method for generating realistic correlation
+    matrices.
+
 @author: Tommy Löfstedt
 @email: tommy.loefstedt@cea.fr
 """
-__all__ = ['ConstantCorrelation']
+__all__ = ['ConstantCorrelation', 'ToeplitzCorrelation']
 
 import numpy as np
 
 
 def ConstantCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
-    """ Returns a positive definite matrix, corresponding to a block covariance
-    matrix. Each block has the structure:
+    """ Returns a positive definite matrix, S, corresponding to a block
+    covariance matrix. Each block has the structure:
 
               [1, ..., rho_k]
         S_k = [...,  1,  ...],
@@ -36,9 +41,10 @@ def ConstantCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
     rho  : A scalar or a list of the average correlation between off-diagonal
            elements of S.
 
-    delta: Baseline noise between groups. (Percent of rho.) Only used if K > 1.
+    delta: Baseline noise between groups. (Percent of rho.) Only used if the
+           number of groups is greater than one.
 
-    eps  : Maximum entry-wise random noise. (Pecent of 1-rho.)
+    eps  : Maximum entry-wise random noise. (Pecent of 1.0 - rho.)
 
     Returns
     -------
@@ -50,7 +56,7 @@ def ConstantCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
 
     K = len(rho)
 
-    M = 10  # Dim. of noise space. Higher is slower but may improve randomness?
+    M = 10  # Dim. of noise space. Converges to the normal pdf as M increases?
     N = 0
     rho_min = min(rho)
     rho_max = max(rho)
@@ -60,7 +66,8 @@ def ConstantCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
     for k in xrange(K):
         N += p[k]
 
-    u = (np.random.rand(M, N) - 0.5) * 2.0  # [-1,1]
+#    u = (np.random.rand(M, N) - 0.5) * 2.0  # ~U(-1, 1)
+    u = np.random.randn(M, N)  # ~N(0, 1)
     u /= np.sqrt(np.sum(u ** 2.0, axis=0))  # Normailse
     uu = np.dot(u.T, u)
 
@@ -84,22 +91,24 @@ def ConstantCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
     return S
 
 
-def ToeplitzCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
-    """ Returns a positive definite matrix, corresponding to a block covariance
-    matrix. Each block has the structure:
+def ToeplitzCorrelation(p=[100], rho=[0.05], eps=0.01):
+    """ Returns a positive definite matrix, S, corresponding to a block
+    covariance matrix. Each block has the structure:
 
-              [1, ..., rho_k]
-        S_k = [...,  1,  ...],
-              [rho_k, ..., 1]
+              [            1,       rho_k^1, rho_k^2,     ..., rho_k^{p_k-1}]
+              [      rho_k^1,             1, rho_k^1,     ..., rho_k^{p_k-2}]
+        S_k = [      rho_k^2,       rho_k^1,       1,     ...,           ...]
+              [          ...,           ...,     ...,       1,       rho_k^1]
+              [rho_k^{p_k-1}, rho_k^{p_k-2},     ..., rho_k^1,             1]
 
-    i.e. 1 on the diagonal and rho_k (on average) outside the diagonal. S then
-    has the structure:
+    i.e. 1 on the diagonal and exponentially decreasing correlations outside
+    the diagonal. S then has the structure:
 
-            [S_1, delta, delta]
-        S = [delta, S_i, delta],
-            [delta, delta, S_N]
+            [S_1,     0,     0]
+        S = [  0,   S_i,     0],
+            [  0,     0,   S_N]
 
-    i.e. with the groups-correlation matrices on the diagonal and delta (on
+    i.e. with the group-correlation matrices on the diagonal and zero (on
     average) outside.
 
     Parameters
@@ -110,9 +119,7 @@ def ToeplitzCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
     rho  : A scalar or a list of the average correlation between off-diagonal
            elements of S.
 
-    delta: Baseline noise between groups. (Percent of rho.) Only used if K > 1.
-
-    eps  : Maximum entry-wise random noise. (Pecent of 1-rho.)
+    eps  : Maximum entry-wise random noise. (Pecent of 1.0 - rho.)
 
     Returns
     -------
@@ -124,37 +131,39 @@ def ToeplitzCorrelation(p=[100], rho=[0.05], delta=0.10, eps=0.01):
 
     K = len(rho)
 
-    M = 10  # Dim. of noise space. Higher is slower but may improve randomness?
+    M = 10  # Dim. of noise space. Converges to the normal pdf as M increases?
     N = sum(p)
     rho_max = max(rho)
     eps = eps * (1.0 - rho_max) / (1.0 + rho_max)
 
-    u = (np.random.rand(M, N) - 0.5) * 2.0  # [-1,1]
+#    u = (np.random.rand(M, N) - 0.5) * 2.0  # ~U(-1, 1)
+    u = np.random.randn(M, N)  # ~N(0, 1)
     u /= np.sqrt(np.sum(u ** 2.0, axis=0))  # Normailse
     uu = np.dot(u.T, u)
 
-    Sigma = np.zeros(N, N)
+    S = np.zeros((N, N))
+    Nk = 0
     for k in xrange(K):
         pk = p[k]
+        Nk += pk
         rhok = rho[k]
         v = [0] * (pk - 1)
         for i in xrange(0, pk - 1):
-            v[i] = rhok ** i
+            v[i] = rhok ** (i + 1)
 
-        Sigmak = np.eye(pk, pk)
-        for i in xrange(0, pk):
-            Sigmak[i]
-
-        uuk = uu[Nk - pk:Nk, Nk - pk:Nk]
-        Sk = rho[k] + eps * uuk
-        Sk -= np.diag(np.diag(Sk)) - np.eye(*Sk.shape)
+        Sk = np.eye(pk, pk)
+        Sk[0, 1:] = v
+        Sk[1:, 0] = v
+        for i in xrange(1, pk - 1):
+            Sk[i, i + 1:] = v[:-i]
+            Sk[i + 1:, i] = v[:-i]
 
         S[Nk - pk:Nk, Nk - pk:Nk] = Sk
 
-    S = Sigma + eps * (uu - np.eye(*uu.shape))
+    S += eps * (uu - np.eye(*uu.shape))
 
-#    k = (N * (1.0 + eps) + 1) / (1.0 - rho_max - eps)
-#    print "cond(S)  = ", np.linalg.cond(S)
-#    print "cond(S) <= ", k
+#    k = (((1.0 + rho_max) / (1.0 - rho_max)) + (N - 1.0) * eps) \
+#         / (((1.0 - rho_max) / (1.0 + rho_max)) - eps)
+#    print "cond(S) = %.5f <= %.5f" % (np.linalg.cond(S), k)
 
     return S
--- structured/tests/data/simulated/regression.py
+++ structured/tests/data/simulated/regression.py
@@ -13,7 +13,7 @@ __all__ = ['load', 'labels']
 import numpy as np
 
 
-def load(size=[100, 100], rho=0.05, delta=0.1, eps=0.01):
+def load(size=[100, 100], rho=0.05, delta=0.1, eps=0.01, locally_smooth=False):
 
     if not isinstance(rho, (list, tuple)):
         size = [size]
@@ -30,7 +30,10 @@ def load(size=[100, 100], rho=0.05, delta=0.1, eps=0.01):
         pk = size[k][1]
         p[k] = pk
 
-    S = correlation_matrices.ConstantCorrelation(p, rho, delta, eps)
+    if locally_smooth:
+        S = correlation_matrices.ToeplitzCorrelation(p, rho, eps)
+    else:
+        S = correlation_matrices.ConstantCorrelation(p, rho, delta, eps)
 
     mean = np.zeros(sum(p))
     X = np.random.multivariate_normal(mean, S, n)
