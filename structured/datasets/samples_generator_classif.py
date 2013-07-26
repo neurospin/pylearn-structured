4ac4058ddf5422554e1d950ccb9876786eebbf12
duchesnay <edouard.duchesnay@gmail.com>
2013-07-26 18:26:23 +0200

    ENH: Sample generator: add supressor variable + control for SNR

--- structured/datasets/samples_generator_classif.py
+++ structured/datasets/samples_generator_classif.py
@@ -9,11 +9,18 @@ import numpy as np
 import scipy.linalg
 
 
-def make_classification(n_samples=100, n_complementary_patterns=1,
-                        size_complementary_patterns=2, n_redundant_patterns=1,
-                        size_redundant_patterns=2, n_independant_features=2,
+def make_classification(n_samples=100,
+                        n_complementary_patterns=1, size_complementary_patterns=2,
+                        n_suppressor_patterns=1,
+                        n_redundant_patterns=1, size_redundant_patterns=2,
+                        n_independant_features=2,
                         snr=3., grp_proportion=.5, n_noize=None):
-    """
+    """ Build classification samples.
+
+    The SNR is distributed accross informative features such that the mean
+    of SNR individual feature is constant for all patterns. Thus we can observ
+    additionnal SNR emerging from features covariation.
+
     Parameters
     ----------
 
@@ -30,6 +37,10 @@ def make_classification(n_samples=100, n_complementary_patterns=1,
 
     size_complementary_patterns: int
         The number of features in each pattern. Default 2.
+
+    n_suppressor_patterns: int
+        Number of patterns made of 2 features: one is linked with the target
+        an the other acts as a suppressor variable.
 
     n_redundant_patterns: int
         Number of patterns made of redundant features. In those patterns
@@ -66,75 +77,117 @@ def make_classification(n_samples=100, n_complementary_patterns=1,
     weigths: The weigths of a Fisher's linear discriminant function, knowing
         the true covariance and mean os the data: Sigma^-1 (m1 - m0)
 
-    m0, m1, cov the parameters used to generate the multivariate normal samples.
+    m0, m1, Cov the parameters used to generate the multivariate normal samples.
     """
-    # distribute snr over informative patterns such that feature contribute equally
-    # to the snr
     n_informative_features = \
     n_complementary_patterns * size_complementary_patterns + \
+    n_suppressor_patterns * 2 + \
     n_redundant_patterns * size_redundant_patterns + \
     n_independant_features
 
     snr_per_feature = snr / n_informative_features
-    snr_per_complementary_pattern = size_complementary_patterns * snr_per_feature
-    snr_per_redundant_pattern = size_redundant_patterns * snr_per_feature
 
     var = 1.
-    cov_informative = .5
+    cov = .5
 
     # build covariance matrix and means vectors
-    cov = np.zeros((n_informative_features, n_informative_features))
+    Cov = np.zeros((n_informative_features, n_informative_features))
     m0 = np.zeros(n_informative_features)
     m1 = np.zeros(n_informative_features)
 
-    # n_complementary_patterns
+    def calc_pattern_snr(m0, m1, cov):
+        return np.sqrt(np.dot(np.dot(scipy.linalg.inv(cov), m1 - m0),  
+                              m1 - m0))
+
+    def calc_univ_snrs(m0, m1, var):
+        """Indivudual snrs"""
+        return np.sqrt((m1 - m0) ** 2 / var)
+
+    # Complementary patterns
     idx = 0
     for i in xrange(n_complementary_patterns):
-        bloc = cov[idx:(idx + size_complementary_patterns),
+        bloc = Cov[idx:(idx + size_complementary_patterns),
                    idx:(idx + size_complementary_patterns)]
-        bloc[::] = cov_informative
+        bloc[::] = cov
         bloc[np.diag_indices(bloc.shape[0])] = var
         la, v = scipy.linalg.eig(bloc)
-        la = np.real(la)
-        min_var = np.min(np.abs(la))
-        dmeans = v[:, np.argmin(np.abs(la))].ravel() * np.sqrt(min_var) * \
-            snr_per_complementary_pattern
+        #la = np.real(la)
+        dmeans = v[:, np.argmin(np.abs(la))].ravel()
         m1[idx:(idx + size_complementary_patterns)] += dmeans
+        # Scale the mean of ALL features such that the sum of univariate snrs
+        # is size_complementary_patterns * snr_per_feature
+        snrs = calc_univ_snrs(
+            m0[idx:(idx + size_complementary_patterns)],
+            m1[idx:(idx + size_complementary_patterns)], var)
+        m1[idx:(idx + size_complementary_patterns)] *= \
+            size_complementary_patterns * snr_per_feature * np.sqrt(var) \
+            / snrs.sum()
+        snrs = calc_univ_snrs(
+            m0[idx:(idx + size_complementary_patterns)],
+            m1[idx:(idx + size_complementary_patterns)], var)
+        print "Complementary patterns snrs:", snrs
         idx += size_complementary_patterns
 
-    # n_redundant_patterns
+    # Suppressors patterns
+    for i in xrange(n_suppressor_patterns):
+        bloc = Cov[idx:(idx + 2),
+                   idx:(idx + 2)]
+        bloc[::] = cov
+        bloc[np.diag_indices(bloc.shape[0])] = var
+        m1[idx] += 1
+        # Scale the mean of ONE feature such that the sum of univariate snrs
+        # is 2 * snr_per_feature
+        snrs = calc_univ_snrs(m0[idx:(idx + 2)], m1[idx:(idx + 2)], var)
+        m1[idx] *= 2 * snr_per_feature * np.sqrt(var) / snrs.sum()
+        snrs = calc_univ_snrs(m0[idx:(idx + 2)], m1[idx:(idx + 2)], var)
+        print "Suppressors patterns snrs:", snrs
+        idx += 2
+    # Redundant patterns
     for i in xrange(n_redundant_patterns):
-        bloc = cov[idx:(idx + size_redundant_patterns),
+        bloc = Cov[idx:(idx + size_redundant_patterns),
                    idx:(idx + size_redundant_patterns)]
-        bloc[::] = cov_informative
+        bloc[::] = cov
         bloc[np.diag_indices(bloc.shape[0])] = var
         la, v = scipy.linalg.eig(bloc)
-        la = np.real(la)
-        max_var = np.max(np.abs(la))
-        dmeans = v[:, np.argmax(np.abs(la))].ravel() * np.sqrt(max_var) * \
-            snr_per_redundant_pattern
-        #dmeans = snr_per_feature * 3 * np.sqrt(min_var) * dmeans
+        #la = np.real(la)
+        dmeans = v[:, np.argmax(np.abs(la))].ravel()
         m1[idx:(idx + size_redundant_patterns)] += dmeans
+        # Scale the mean of ALL features such that the sum of univariate snrs
+        # is size_complementary_patterns * snr_per_feature
+        snrs = calc_univ_snrs(
+            m0[idx:(idx + size_redundant_patterns)],
+            m1[idx:(idx + size_redundant_patterns)], var)
+        m1[idx:(idx + size_redundant_patterns)] *= \
+            size_redundant_patterns * snr_per_feature * np.sqrt(var) \
+            / snrs.sum()
+        snrs = calc_univ_snrs(
+            m0[idx:(idx + size_redundant_patterns)],
+            m1[idx:(idx + size_redundant_patterns)], var)
+        print "Redundant patterns snrs:", snrs
+        #dmeans = v[:, np.argmax(np.abs(la))].ravel() * np.sqrt(max_var) * \
+        #    snr_per_feature
+        #m1[idx:(idx + size_redundant_patterns)] += dmeans
         idx += size_redundant_patterns
 
-    # independant features
+    # Independant features
     for i in xrange(n_independant_features):
-        cov[idx, idx] = var
-        m1[idx] = np.sqrt(var) * snr_per_feature
+        Cov[idx, idx] = var
+        m1[idx] = snr_per_feature * np.sqrt(var)
         idx += 1
-
     # Nomalize m1 to scale to the desire global snr
-    snr_actual = np.sqrt(np.dot(np.dot(scipy.linalg.inv(cov), m1),  m1))
+    snr_actual = np.sqrt(np.dot(np.dot(scipy.linalg.inv(Cov), m1 - m0),
+                                m1 - m0))
+    print "Global snr:", snr_actual
     m1 = m1 * snr / snr_actual
 
     n_g0 = int(np.round(n_samples * grp_proportion))
 
-    X0 = np.random.multivariate_normal(m0, cov, n_g0)
-    X1 = np.random.multivariate_normal(m1, cov, n_samples - n_g0)
+    X0 = np.random.multivariate_normal(m0, Cov, n_g0)
+    X1 = np.random.multivariate_normal(m1, Cov, n_samples - n_g0)
     X = np.vstack([X0, X1])
     y = np.array([0] * n_g0 + [1] * (n_samples - n_g0))
 
-    weigths = np.dot((m1 - m0), scipy.linalg.inv(cov))
+    weigths = np.dot((m1 - m0), scipy.linalg.inv(Cov))
 
     # Add noize
     if n_noize is None:
@@ -143,39 +196,82 @@ def make_classification(n_samples=100, n_complementary_patterns=1,
     X = np.hstack([X, np.random.rand(n_samples, n_noize)])
     weigths = np.concatenate((weigths, [0] * n_noize))
 
-    return X, y, weigths, m0, m1, cov
+    return X, y, weigths, m0, m1, Cov
 
 if __name__ == '__main__':
-    X, y, weigths, m0, m1, cov = make_classification(n_samples=500, n_complementary_patterns=1,
-                            size_complementary_patterns=2, n_redundant_patterns=1,
-                            size_redundant_patterns=2, n_independant_features=2,
-                            snr=3., grp_proportion=.5, n_noize=None)
+    X, y, weigths, m0, m1, Cov = make_classification(n_samples=500,
+                        n_complementary_patterns=1,
+                        size_complementary_patterns=2,
+                        n_suppressor_patterns=1,
+                        n_redundant_patterns=1,
+                        size_redundant_patterns=2, n_independant_features=2,
+                        snr=3., grp_proportion=.5, n_noize=None)
+
+    def plot_2d(plot, X, y, xlab, ylab, title):
+        m0_hat = X[y == 0, :].mean(axis=0)
+        m1_hat = X[y == 1, :].mean(axis=0)
+        Xc = X.copy()
+        Xc[y == 0, :] -= m0_hat
+        Xc[y == 1, :] -= m1_hat
+        cov_hat = np.cov(Xc.T)
+        weigths_hat = np.dot((m1_hat - m0_hat), scipy.linalg.inv(cov_hat))
+        snr_glob = np.sqrt(np.dot(np.dot(scipy.linalg.inv(cov_hat),
+                                         m1_hat - m0_hat), m1_hat - m0_hat))
+        snr_glob = round(snr_glob, 2)
+        snrs = np.round(np.sqrt((m1_hat - m0_hat) ** 2 / cov_hat.diagonal()), 2)
+        # plot x, y
+        plt.plot(X[y == 0, 0], X[y == 0, 1], 'or', X[y == 1, 0], X[y == 1, 1],
+                 'ob')
+        # plot means
+        plt.plot(m0_hat[0], m0_hat[1], 'oy', m1_hat[0], m1_hat[1], 'oy',
+                 markersize=18)
+        # plot Cov
+        la, v = scipy.linalg.eig(cov_hat)
+        std_dev = np.sqrt(np.real(la))
+        e0 = patches.Ellipse(m0_hat, 2 * std_dev[0], 2 * std_dev[1],
+                 angle=np.arctan(v[1, 0] / v[0, 0]) * 180. / np.pi,
+                 linewidth=3, fill=False, color='y', linestyle='dashed',
+                 zorder=10)
+        plot.add_patch(e0)
+        e1 = patches.Ellipse(m1_hat, 2 * std_dev[0], 2 * std_dev[1],
+                 angle=np.arctan(v[1, 0] / v[0, 0]) * 180. / np.pi,
+                 linewidth=3, fill=False, color='y', linestyle='dashed',
+                 zorder=10)
+        plot.add_patch(e1)
+        # plot weights
+        plot.arrow(m0_hat[0], m0_hat[1], weigths_hat[0], weigths_hat[1],
+                   head_width=.5, fc="g", ec="g", head_length=.5, linewidth=3,
+                   zorder=10)
+        plt.xlabel(xlab + ': snr=%.2f' % snrs[0])
+        plt.ylabel(ylab + ': snr=%.2f' % snrs[1])
+        plt.title(title + ': snr=%.2f' % snr_glob)
+
+    #plt.show()
 
     import matplotlib.pyplot as plt
+    from matplotlib import patches
 
     proj = np.dot(X, weigths)
 
-    plt.subplot(221)
-    plt.plot(X[y == 0, 0], X[y == 0, 1], 'or', X[y == 1, 0], X[y == 1, 1], 'ob')
-    plt.xlabel("X0")
-    plt.ylabel("X1")
-    plt.title('Complementary pattern')
-
-    plt.subplot(222)
-    plt.plot(X[y == 0, 2], X[y == 0, 3], 'or', X[y == 1, 2], X[y == 1, 3], 'ob')
-    plt.xlabel("X2")
-    plt.ylabel("X3")
-    plt.title('Redundant pattern')
-
-    plt.subplot(223)
-    plt.plot(X[y == 0, 4], X[y == 0, 5], 'or', X[y == 1, 4], X[y == 1, 5], 'ob')
-    plt.xlabel("X4")
-    plt.ylabel("X5")
-    plt.title('Independant features')
-
-    plt.subplot(224)
-    plt.plot(proj[y==0], 'or', proj[y==1], 'ob')
-    plt.xlabel("X'w")
-    plt.title('Projection on discriminative axis')
-
+    plt.figure(figsize=(10, 10))
+    idx = 0
+    plot = plt.subplot(221)
+    plot_2d(plot=plot, X=X[:, idx:(idx + 2)], y=y,
+        xlab="X" + str(idx), ylab="X" + str(idx + 1),
+        title='Complementary pattern')
+    idx += 2
+    plot = plt.subplot(222)
+    plot_2d(plot=plot, X=X[:, idx:(idx + 2)], y=y,
+        xlab="X" + str(idx), ylab="X" + str(idx + 1),
+        title='Suppressor variables')
+    idx += 2
+    plot = plt.subplot(223)
+    plot_2d(plot=plot, X=X[:, idx:(idx + 2)], y=y,
+        xlab="X" + str(idx), ylab="X" + str(idx + 1),
+        title='Redundant pattern')
+    idx += 2
+    plot = plt.subplot(224)
+    plot_2d(plot=plot, X=X[:, idx:(idx + 2)], y=y,
+        xlab="X" + str(idx), ylab="X" + str(idx + 1),
+        title='Independant features')
     plt.show()
