a2efb4e764a3a1c220364299dddccafe0857c5ca
duchesnay <edouard.duchesnay@gmail.com>
2013-08-31 23:42:59 +0200

    add in __init__

--- structured/datasets/__init__.py
+++ structured/datasets/__init__.py
@@ -6,5 +6,7 @@ Created on Mon Jul 29 17:16:07 2013
 """
 
 from .samples_generator_nostruct import make_classification
+from .samples_generator_struct import make_regression_struct
 
-__all__ = ['make_classification']
+
+__all__ = ['make_classification' 'make_regression_struct']
