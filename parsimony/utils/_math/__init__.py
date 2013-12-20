1db13ec8b3bc17d650a7d5a932e04e0b1ee83f70
duchesnay <edouard.duchesnay@gmail.com>
2013-12-21 00:12:16 +0100

    comment utils.math.cov

--- parsimony/utils/_math/__init__.py
+++ parsimony/utils/_math/__init__.py
@@ -12,7 +12,6 @@ from .math import norm1
 from .math import norm_inf
 from .math import norm0
 from .math import sign
-from .math import cov
 from .math import corr
 
-__all__ = ['norm', 'norm1', 'norm_inf', 'norm0', 'sign', 'cov', 'corr']
+__all__ = ['norm', 'norm1', 'norm_inf', 'norm0', 'sign', 'corr']
