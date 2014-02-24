643aa32364596d26ab984a9ebbfe13ec8403b8df
duchesnay <edouard.duchesnay@gmail.com>
2014-02-24 01:19:32 +0100

    ENH: plot 2D weigths map, add limits

--- parsimony/utils/plot.py
+++ parsimony/utils/plot.py
@@ -7,23 +7,31 @@ Created on Mon Jan  6 14:13:42 2014
 import matplotlib.pyplot as plt
 import numpy as np
 
-def plot_map2d(map2d, plot=None, title=None):
+def plot_map2d(map2d, plot=None, title=None, limits=None, center_cmap=True):
     if plot is None:
         plot = plt
     map2d = map2d.squeeze()
     if len(map2d.shape) != 2:
         raise ValueError("input map is not 2D")
+    if np.asarray(limits).size is 2:
+        mx = limits[0]
+        mi = limits[1]
+    else:
+        mx = map2d.max()
+        mi = map2d.min()
+    if center_cmap:
+        mx = np.abs([mi, mx]).max()
+        mi = -mx
     cax = plot.matshow(map2d, cmap=plt.cm.coolwarm)
     frame = plt.gca()
     frame.get_xaxis().set_visible(False)
     frame.get_yaxis().set_visible(False)
-    mx = np.abs(map2d).max()
-    k = 1
-    while (10 ** k * mx) < 1 and k < 10:
-        k += 1
-    ticks = np.array([-mx, -mx / 4 - mx / 2, 0, mx / 2, mx / 2,
-                      mx]).round(k + 2)
-    cbar = plt.colorbar(cax, ticks=ticks)
-    cbar.set_clim(vmin=-mx, vmax=mx)
+    #k = 1
+    #while (10 ** k * mx) < 1 and k < 10:
+    #    k += 1
+    #ticks = np.array([-mi, -mi / 4 - mi / 2, 0, mx / 2, mx / 2,
+    #                  mx]).round(k + 2)
+    cbar = plt.colorbar(cax)#, ticks=ticks)
+    cbar.set_clim(vmin=mi, vmax=mx)
     if title is not None:
         plt.title(title)
